28974aa26b861acf1cb9ac5606e931908a656e76
Väinö <vainoelias.ikonen@gmail.com>
2026-04-21 13:16:30 +0300

    Charge attack effect

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -4,12 +4,16 @@ public class Shooting : MonoBehaviour
 {
     public GameObject bulletPrefab;
     public Transform firePoint;
+    public Transform firePoint2;
+    public Transform firePoint3;
     public float fireForce;
     public float fireDelay;
     private float delayTimer;
     Vector3 clickPosition;
     public Animator animator;
     bool charging;
+    public ParticleSystem chargeParticles;
+    public ParticleSystem chargeBloom;
     
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -31,11 +35,15 @@ public class Shooting : MonoBehaviour
         {
             animator.SetBool("Charge", true);
             charging = true;
+            chargeParticles.Play();
+            chargeBloom.Play();
         }
         if (Input.GetKeyUp(KeyCode.Mouse1))
         {
             animator.SetBool("Charge", false);
             charging = false;
+            chargeParticles.Stop();
+            chargeBloom.Stop();
         }
     }
     public void Fire()
@@ -52,9 +60,39 @@ public class Shooting : MonoBehaviour
         GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
         bullet.GetComponent<Rigidbody>().AddForce(firePoint.forward.normalized * fireForce, ForceMode.Impulse);
     }
+    public void Fire2()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Plane plane = new Plane(Vector3.up, transform.position);
+
+        if (plane.Raycast(ray, out float distance))
+        {
+            clickPosition = ray.GetPoint(distance);
+        }
+        Vector3 bulletDirection = new Vector3(clickPosition.x - firePoint.position.x, 0f, clickPosition.z - firePoint.position.z);
+        //Vector3 bulletDirection = clickPosition - firePoint.position;
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        bullet.GetComponent<Rigidbody>().AddForce(firePoint2.forward.normalized * fireForce, ForceMode.Impulse);
+    }
+    public void Fire3()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Plane plane = new Plane(Vector3.up, transform.position);
+
+        if (plane.Raycast(ray, out float distance))
+        {
+            clickPosition = ray.GetPoint(distance);
+        }
+        Vector3 bulletDirection = new Vector3(clickPosition.x - firePoint.position.x, 0f, clickPosition.z - firePoint.position.z);
+        //Vector3 bulletDirection = clickPosition - firePoint.position;
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        bullet.GetComponent<Rigidbody>().AddForce(firePoint3.forward.normalized * fireForce, ForceMode.Impulse);
+    }
     public void ChargedFire()
     {
         Fire();
+        Fire2();
+        Fire3();
         animator.SetBool("Charge", false);
         charging = false;
     }
